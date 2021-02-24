31119e2cda6771fe10ca7bcb68a9d8404c075da7
Wraithname <43811054+Wraithname@users.noreply.github.com>
2021-02-24 12:39:44 +0300

    Форма работы

--- TestingAndLerning/Form1.Designer.cs
+++ TestingAndLerning/Form1.Designer.cs
@@ -114,6 +114,7 @@ namespace TestingAndLerning
             this.button1.TabIndex = 1;
             this.button1.Text = "Выбор папки с изображениями";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
